8913091368b1d47a9c790c538c15cce32da7be04
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-28 21:56:16 +0800

    reentrant lock &AtomicInteger.addAndGet() (#80)
    reentrant lock &AtomicInteger.addAndGet() (#80)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,44 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
-    private int value = 0;
+    /*我们有一个在多线程环境下不安全的Counter类，请将它改写为线程安全的。
+
+    我们鼓励你使用多种方法：
+
+    synchronized方法
+    java.util.concurrent.locks.Lock
+    AtomicInteger
+    祝你好运！*/
+    private static AtomicInteger value = new AtomicInteger(0);
+    private int valueForLockTest = 0;
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
-        return value;
+        return value.get();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        return value.addAndGet(i);
+    }
+
+    public int addAndGetWithLock(int i) {
+        lock.lock();  // block until condition holds
+        try {
+            valueForLockTest += i;
+            return valueForLockTest;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        return value.addAndGet(-i);
     }
+
 }
