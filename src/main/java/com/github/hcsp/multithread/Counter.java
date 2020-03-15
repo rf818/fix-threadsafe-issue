918b35dfd7cbaebd53d33ebeab0c36421e73f1b3
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2020-03-15 23:01:37 +0800

    线程安全修改 (#84)
    线程安全修改 (#84)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,47 @@
 package com.github.hcsp.multithread;
 
+//import java.util.concurrent.atomic.AtomicInteger;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
+    // private AtomicInteger value = new AtomicInteger();
     private int value = 0;
+    private Lock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
+        // return value.get();
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        value += i;
-        return value;
+    int addAndGet(int i) {
+        //value += i;
+        lock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
+
+        //  return value;
+
+        //return value.addAndGet(i);
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        value -= i;
-        return value;
+    synchronized int minusAndGet(int i) {
+        lock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
+        //value -= i;
+
+        //return value.addAndGet(-i);
     }
 }
