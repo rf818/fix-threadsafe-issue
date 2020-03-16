1c70384dcdc0af1c8391e62e4e8df749259d4c19
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-03-16 16:10:42 +0800

    三种方法完成需求 (#85)
    三种方法完成需求 (#85)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,64 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private int value = 0;
+    private AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
+
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        return value.addAndGet(i);
+
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        return value.addAndGet(-i);
     }
+
 }
+
+/**
+ *  ** method 1 **
+ *
+ * public synchronized int addAndGet(int i) {
+ *  value += i;
+ *  return value;
+ * }
+ *
+ * public synchronized int minusAndGet(int i) {
+ *   value -= i;
+ *   return value;
+ * }
+ *
+ *
+ * ** method 2 **
+ * public Lock lock = new ReentrantLock();
+ *
+ * // 加上一个整数i，并返回加之后的结果
+ * public int addAndGet(int i) {
+ *   try {
+ *     lock.lock();
+ *     value += i;
+ *      return value;
+ *   } finally {
+ *     lock.unlock();
+ *   }
+ * }
+ *
+ * // 减去一个整数i，并返回减之后的结果
+ * public int minusAndGet(int i) {
+ *   try {
+ *      lock.lock();
+ *      value -= i;
+ *      return value;
+ *   } finally {
+ *     lock.unlock();
+ *   }
+ * }
+ */
