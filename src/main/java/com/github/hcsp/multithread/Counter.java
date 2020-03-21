01a45f88c372ae7c1d3cc6dd06237fb917c21081
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-03-21 18:29:43 +0800

    修改Counter类使其线程安全 (#90)
    修改Counter类使其线程安全 (#90)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
@@ -9,13 +13,57 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
+        lock.lock();
         value += i;
+        lock.unlock();
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
+        lock.lock();
+        value -= i;
+        lock.unlock();
+        return value;
+    }
+}
+
+class Counter2 {
+    private int value = 0;
+
+    public int getValue() {
+        return value;
+    }
+
+    // 加上一个整数i，并返回加之后的结果
+    public synchronized int addAndGet(int i) {
+        value += i;
+        return value;
+    }
+
+    // 减去一个整数i，并返回减之后的结果
+    public synchronized int minusAndGet(int i) {
         value -= i;
         return value;
+    }
+}
+
+class Counter3 {
+    private AtomicInteger value = new AtomicInteger(0);
+
+    public int getValue() {
+        return value.intValue();
+    }
+
+    // 加上一个整数i，并返回加之后的结果
+    public int addAndGet(int i) {
+        value.addAndGet(i);
+        return value.intValue();
+    }
+
+    // 减去一个整数i，并返回减之后的结果
+    public int minusAndGet(int i) {
+        value.addAndGet(-i);
+        return value.intValue();
     }
 }
