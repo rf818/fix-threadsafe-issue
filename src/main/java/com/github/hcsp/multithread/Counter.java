4b35be58b860df0716be443996f223d1bbe1f1b3
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2020-03-31 02:16:57 -0700

    多线程练习：解决一个类的线程安全问题 (#96)
    多线程练习：解决一个类的线程安全问题 (#96)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,28 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private int value = 0;
+    private AtomicInteger value = new AtomicInteger();
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        value.addAndGet(i);
+        return value.intValue();
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        value.addAndGet(-i);
+        return value.intValue();
+    }
+
+    public static void main(String args[]) {
+        Counter counter = new Counter();
+        System.out.println(counter.addAndGet(2));
     }
 }
