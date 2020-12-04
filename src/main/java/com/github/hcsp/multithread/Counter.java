087a9cc52f66c8b0cf28857c26be51e5e2dd54fb
pccmast <35651458+pccmast@users.noreply.github.com>
2020-12-04 21:46:24 +0800

    Pcc 3 (#145)
    Pcc 3 (#145)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
 
@@ -7,15 +9,27 @@ public class Counter {
         return value;
     }
 
+    ReentrantLock lock = new ReentrantLock();
+
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        lock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        lock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
