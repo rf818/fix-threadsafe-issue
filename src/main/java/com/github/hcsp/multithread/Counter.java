661b435c9822a7cd7937fc1bf7827e1584b44cd0
chengpenggm <79912150+chengpenggm@users.noreply.github.com>
2021-03-24 15:47:28 +0800

    加锁实现线程安全 (#151)
    加锁实现线程安全 (#151)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,45 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Counter {
     private int value = 0;
 
+    private static ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static Lock readLock = reentrantReadWriteLock.readLock();
+    private static Lock writeLock = reentrantReadWriteLock.writeLock();
+
+
     public int getValue() {
-        return value;
+        readLock.lock();
+        try {
+            return value;
+        } finally {
+            readLock.unlock();
+        }
+
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        writeLock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        writeLock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            writeLock.unlock();
+        }
     }
 }
