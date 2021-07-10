e494ca9bc25e5493281400a2182af87da0903960
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-07-10 17:54:53 +0800

    Feature/task (#162)
    Feature/task (#162)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.multithread;
 
 public class Counter {
+    private final Object lock = new Object();
     private int value = 0;
 
     public int getValue() {
@@ -9,13 +10,17 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
+        synchronized (lock) {
+            value += i;
+        }
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
+        synchronized (lock) {
+            value -= i;
+        }
         return value;
     }
 }
