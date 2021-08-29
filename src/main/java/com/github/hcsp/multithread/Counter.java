e7d531a0170dc26b95dbf60d1b94730414a1d65c
jianqibot <85292386+jianqibot@users.noreply.github.com>
2021-08-29 20:24:02 +1000

    job done using second method (#167)
    job done using second method (#167)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,34 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        value += i;
-        return value;
+    public synchronized int addAndGet(int i) {
+        lock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        value -= i;
-        return value;
+    public synchronized int minusAndGet(int i) {
+        lock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
