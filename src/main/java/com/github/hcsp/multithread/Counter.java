9b15a9e4db98fb278d6ccf6f001b3044718c5bc5
ang tsang <tsang1205@qq.com>
2022-08-07 21:12:35 +0800

    fix: threadsafe issue (#188)
    fix: threadsafe issue (#188)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,66 @@
 package com.github.hcsp.multithread;
 
+//import java.util.concurrent.atomic.AtomicInteger;
+
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
-    private int value = 0;
+    private Integer value = 0;
+
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    // 加上一个整数i，并返回加之后的结果
+//    public synchronized int addAndGet(int i) {
+//        value += i;
+//        return value;
+//    }
+//
+//    // 减去一个整数i，并返回减之后的结果
+//    public synchronized int minusAndGet(int i) {
+//        value -= i;
+//        return value;
+//    }
+
+    //    private AtomicInteger value = new AtomicInteger(0);
+//
+//    public int getValue() {
+//        return value.get();
+//    }
+//
+//    // 加上一个整数i，并返回加之后的结果
+//    public int addAndGet(int i) {
+//        return value.addAndGet(i);
+//    }
+//
+//    // 减去一个整数i，并返回减之后的结果
+//    public int minusAndGet(int i) {
+//        return value.addAndGet(-i);
+//    }
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
     }
 
-    // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        try {
+            lock.lock();
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        try {
+            lock.lock();
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
